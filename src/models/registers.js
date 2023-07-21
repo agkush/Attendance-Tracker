3efd2baf2abd90391280e21ce695710f187c213c
Kushagra Agarwal <agkush2001@gmail.com>
2023-07-21 22:40:18 +0530

    login changes

--- src/models/registers.js
+++ src/models/registers.js
@@ -28,6 +28,60 @@ const studentSchema = new mongoose.Schema({
         required:true,
         unique:true
     },
+    subject1 : {
+        type : String,
+    },
+    subject2 : {
+        type : String,
+    },
+    subject3 : {
+        type : String,
+    },
+    subject4 : {
+        type : String,
+    },
+    subject5 : {
+        type : String,
+    },
+    subject6 : {
+        type : String,
+    },
+    present1 : {
+        type : Number,
+    },
+    absent1 : {
+        type : Number,
+    },
+    present2 : {
+        type : Number,
+    },
+    absent2 : {
+        type : Number,
+    },
+    present3 : {
+        type : Number,
+    },
+    absent3 : {
+        type : Number,
+    },
+    present4 : {
+        type : Number,
+    },
+    absent4 : {
+        type : Number,
+    },
+    present5 : {
+        type : Number,
+    },
+    absent5 : {
+        type : Number,
+    },
+    present6 : {
+        type : Number,
+    },
+    absent6 : {
+        type : Number,
+    },
     password : {
         type : String,
         required:true
